8e6ecc2fd43229b0d570a6da8166e1b7919f22f9
gdm-1718-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2018-10-21 22:33:01 +0200

    Update custom.js
    From now on you only have to add this script to get started.

--- custom.js
+++ custom.js
@@ -1,3 +1,16 @@
+addCSS('style.css');
+
+// Include CSS file
+function addCSS(filename){
+    var head = document.getElementsByTagName('head')[0];
+
+    var style = document.createElement('link');
+    style.href = 'https://lennertderyck.github.io/custom-elements/elements.css';
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    head.append(style);
+}
+
 function collapse(div) {
     var className = div.getAttribute("class");
     if(className=="collapse-area") {
